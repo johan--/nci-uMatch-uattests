4b4f7b04ffaba79b283412f0c6481ee56b13ec3c
Ramani, Vivek (NIH/NCI) [C] <ramaniv@mail.nih.gov>
2016-12-01 16:20:26 -0500

    webdriver manager issue

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ exports.config = {
     baseUrl: process.env.UI_HOSTNAME,  //when developing tests use http://localhost:9000',
 
     //seleniumAddress: 'http://localhost:4444/wd/hub',
-    //chromeOnly: true,
+    chromeOnly: true,
     //directConnect: true,
     capabilities: {
         browserName: 'chrome',
